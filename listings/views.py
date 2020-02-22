a4443ac0b7b6f49cef3657d89b65c293b6e27aec
ValGalesnik <validol27@gmail.com>
2020-02-22 19:21:36 +0300

    Local settings file for production server

--- listings/views.py
+++ listings/views.py
@@ -13,7 +13,7 @@ CACHE_TTL = 30*60
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-    paginator = Paginator(listings, 6)
+    paginator = Paginator(listings, 3)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
 
