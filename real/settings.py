72c308548f53c99911f1b44ce76d8e3e6a9ca5b0
Val Galesnik <validol27@gmail.com>
2020-02-22 23:31:10 +0300

    Update settings.py

--- real/settings.py
+++ real/settings.py
@@ -152,14 +152,14 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 
 # REDIS
 #CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
-        },
-        "KEY_PREFIX": "example"
-    }
+    #"default": {
+        #"BACKEND": "django_redis.cache.RedisCache",
+        #"LOCATION": "redis://127.0.0.1:6379/1",
+        #"OPTIONS": {
+            #"CLIENT_CLASS": "django_redis.client.DefaultClient"
+        #},
+       # "KEY_PREFIX": "example"
+   # }
 }
 
 # SESSIONS
